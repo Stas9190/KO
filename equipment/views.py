eac8ccbea1298f10e857bd17175e4cf5a3c923bd
Stas <shulga_stas@mail.ru>
2018-03-02 15:29:21 +0300

    добавил тестовый шаблон

--- equipment/views.py
+++ equipment/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import equipmentType
 
 # Список доступного оборудования
 def equipment_list(request):
-    return render(request, 'equipment_list.html', {})
+    equipment = equipmentType.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request, 'equipment_list.html', {'equipment': equipment})
+
